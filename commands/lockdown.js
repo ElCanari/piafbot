fec395eb3831af95b2fed4ae08dbf1d0b234079d
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-02-21 14:53:40 -0500

    Add files via upload

---
+++ commands/lockdown.js
@@ -0,0 +1,36 @@
+const ms = require('ms');
+exports.run = (client, message, args) => {
+  if (!client.lockit) client.lockit = [];
+  let time = args.join(' ');
+  let validUnlocks = ['release', 'unlock'];
+  if (!time) return message.reply('Tu dois mêtre du temp en seconde ou minute');
+
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.sendMessage('Lockdown lifted.');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.sendMessage(`Channel locked pour ${ms(ms(time), { long:true })}`).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.sendMessage('Lockdown arreter')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+};
